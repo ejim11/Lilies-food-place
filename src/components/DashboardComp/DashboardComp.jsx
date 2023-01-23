cbeef595cae5368363b4a02d5f8b4878f1f7aca0
Ejim Favour <favourejim56@gmail.com>
2023-01-23 08:42:25 +0100

    chore: enabled the modal to be visible in all pages

--- src/components/DashboardComp/DashboardComp.jsx
+++ src/components/DashboardComp/DashboardComp.jsx
@@ -1,12 +1,23 @@
 import classes from "./DashboardComp.module.scss";
 import DashboardLinks from "./DashboardLinks/DashboardLinks";
 import { Outlet } from "react-router-dom";
+import Cart from "./Cart/Cart";
+import Order from "./Order/Order";
+import AddToCart from "./AddToCart/AddToCart";
+import { useSelector } from "react-redux";
 
 const DashboardComp = () => {
+  const cartLinkState = useSelector((state) => state.cart.cartLink);
+
+  const addToCartId = useSelector((state) => state.cart.addToCartId);
+
   return (
     <main className={classes.main}>
       <DashboardLinks />
       <Outlet />
+      {cartLinkState === "cart" && <Cart />}
+      {cartLinkState === "order" && <Order />}
+      {addToCartId && <AddToCart />}
     </main>
   );
 };
