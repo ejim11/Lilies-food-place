e79c88b2535fa8e5c8d62abf7b76cc0b87babc2f
Ejim Favour <favourejim56@gmail.com>
2023-02-05 00:28:08 +0100

    modified the meal item page

--- src/components/UI/GridBoxAndItem/MealItem.jsx
+++ src/components/UI/GridBoxAndItem/MealItem.jsx
@@ -3,32 +3,47 @@ import { useDispatch } from "react-redux";
 import { cartActions } from "../../../store/cartSlice";
 import Button from "../Button/Button";
 import classes from "./MealItem.module.scss";
+import { useSelector } from "react-redux";
+import { BsFillHeartFill } from "react-icons/bs";
+import { useState } from "react";
 
 const MealItem = ({ meal }) => {
   const dispatchFn = useDispatch();
-  console.log(meal);
+
+  const [favourite, setFavourite] = useState(false);
+
+  const userTypeState = useSelector((state) => state.auth.userType);
+
+  const toggleFavourite = () => {
+    setFavourite(prevState => !prevState)
+  }
 
   return (
     <li className={classes["meal-item"]}>
+      <BsFillHeartFill  className={`${favourite ? classes["favourite-true"]: classes["favourite-false"]} ${classes["favourite-icon"]}`} onClick = {toggleFavourite}/>
       <div className={classes["meal-img"]}>
-        <img
-          src={`https://lilliesfoodpalace.kazcodez.dev/${meal.meal_avatar}`}
-          alt={meal.meal_name}
-        />
+        <img src={`${meal.meal_avatar}`} alt={meal.meal_name} />
       </div>
       <h3>{meal.meal_name}</h3>
       <p className={classes["meal-text"]}>{meal.meal_details}</p>
       <div className={classes["meal-amount-box"]}>
         <p>N {modifyNum(String(meal.meal_price))}</p>
-        <Button
-          type={"button"}
-          className={classes["btn-add-to-cart"]}
-          onClick={() => {
-            dispatchFn(cartActions.changeAddToCartId(meal.unique_id));
-          }}
-        >
-          Add to cart
-        </Button>
+        {userTypeState !== "vendor" && (
+          <Button
+            type={"button"}
+            className={classes["btn-add-to-cart"]}
+            onClick={() => {
+              dispatchFn(cartActions.changeAddToCartId(meal.unique_id));
+            }}
+          >
+            Add to cart
+          </Button>
+        )}
+        {userTypeState === "vendor" && (
+          <Button type={"button"} className={classes["btn-add-to-cart"]}>
+            Update meal
+          </Button>
+        )}
       </div>
     </li>
   );
