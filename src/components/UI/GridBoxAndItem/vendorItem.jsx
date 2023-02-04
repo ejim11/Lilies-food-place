77b6c4047366ea7a5c27265873ffce168df5c0a8
Ejim Favour <favourejim56@gmail.com>
2023-02-05 00:28:58 +0100

    modified the vendor item page

--- src/components/UI/GridBoxAndItem/vendorItem.jsx
+++ src/components/UI/GridBoxAndItem/vendorItem.jsx
@@ -6,14 +6,21 @@ const VendorItem = ({ vendor }) => {
 
   return (
     <li className={classes["vendor-item"]}>
-      <img src={vendor.vendor_avatar} alt={vendor.company_name} />
-      <p>vendor name: {vendor.company_name}</p>
-      <p>email: {vendor.email}</p>
-      <p>phone: {vendor.phone}</p>
+      <div className={classes["vendor-img"]}>
+        <img src={vendor.vendor_avatar} alt={vendor.company_name} />
+      </div>
+      <h3 className={classes["vendor-name"]}>{vendor.company_name}</h3>
+      <p className={classes["vendor-text"]}>
+        <span>Email:</span> {vendor.email}
+      </p>
+      <p className={classes["vendor-text"]}>
+        <span>Phone:</span> {vendor.phone}
+      </p>
       <button
         onClick={() => {
           navigate(`${vendor.unique_id}`);
         }}
+        className={classes.btn}
       >
         View Vendor
       </button>
